3a31a3c9b01fece83b429eda128bab422333d895
ABİDE BEYZA BAYRAM <abidebeyzabayram@icloud.com>
2022-09-25 12:53:33 +0300

    hesap makinesi
    hesap makinesi

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+
+	public static void main (String []args) {
+		
+	
+	ReCapdemo reCapdemo = new ReCapdemo();
+	int sonuc = reCapdemo.Topla(34, 50) ;
+	System.out.println(sonuc);
+	
+	}
+
+}
---
+++ src/ReCapdemo.java
@@ -0,0 +1,24 @@
+
+public class ReCapdemo{
+
+ public int Topla(int sayı1,int sayı2) {
+	 return sayı1+sayı2;
+ }
+	
+ public int Cıkar(int sayı1,int sayı2) {
+	 return sayı1-sayı2;		
+		
+ }	
+		
+ public int Carp(int sayı1,int sayı2) {
+	 return sayı1*sayı2;	
+ 
+ 
+	}
+
+ public int Bol(int sayı1,int sayı2) {
+	 return sayı1/sayı2;
+ }
+}
+ 
+
